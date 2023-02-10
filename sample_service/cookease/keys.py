1f7f90d42281568c86cadb4a8a7db3a2e49d9eb5
jennifer <jennifertovar2010@gmail.com>
2023-02-09 19:01:00 -0600

    moved around keys file

--- sample_service/cookease/keys.py
+++ sample_service/cookease/keys.py
@@ -1,1 +1,5 @@
-SPOONACULAR_API_KEY = "0ceb97e00e73444fb5327c838f8bddbb"
+# SPOONACULAR_API_KEY = "0ceb97e00e73444fb5327c838f8bddbb"
+
+import os
+key = os.environ.get('SPOONACULAR_API_KEY')
+print(key)
