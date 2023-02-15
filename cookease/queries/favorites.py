bb8af1d9fb1825b50e5833028f855d8f509c0bed
Eric <aasian214@gmail.com>
2023-02-15 13:44:39 -0500

    started favorites backend

--- cookease/queries/favorites.py
+++ cookease/queries/favorites.py
@@ -1,4 +1,29 @@
 from .client import Queries
+from pydantic import BaseModel
+
+
+class FavoriteIn(BaseModel):
+    recipe_id: int
+
+class FavoriteOut(BaseModel):
+    user_id: int
+    recipe_id: int
+    id: int
 
 class FavoritesQueries(Queries):
-    pass
+    DB_NAME = "db-name"
+    COLLECTION = "favorites"
+
+
+    def get_favorite(self, id: int) -> FavoriteOut:
+        favorites = self.collection.find_one({"id": id})
+        if not favorites:
+            return None
+        favorites["id"] = str(favorites["_id"])
+        return FavoriteOut(**favorites)
+
+    def create_favorite(self, favorite: FavoriteIn) -> FavoriteOut:
+        favorites = favorite.dict()
+        self.collection.insert_one(favorites)
+        favorites["id"] = str(favorites["_id"])
+        return FavoriteOut(recipes = [], **favorites)
--- cookease/routers/favorites.py
+++ cookease/routers/favorites.py
@@ -4,14 +4,29 @@ from authenticator import authenticator
 
 from pydantic import BaseModel
 
-from queries.favorites import (FavoritesQueries)
+from queries.favorites import (FavoritesQueries, FavoriteIn, FavoriteOut)
 
 router = APIRouter()
 
 
-@router.get("/favorites", response_model=bool)
-async def get_protected(
-    favorites: FavoritesQueries = Depends(),
-    account_data: dict = Depends(authenticator.get_current_account_data),
+@router.get("/favorite-recipes", response_model=FavoriteOut)
+async def get_favorite(
+    favorite: FavoritesQueries = Depends(),
+    account_data: dict = Depends(authenticator.get_account_data),
 ):
-    return favorites.get_current_account_data(account_data)
+    if "username" in account_data.username:
+        return FavoriteOut(favorites = favorite.get_all())
+    else:
+        return "username does not exist"
+
+@router.post("/favorites-recipes", response_model=FavoriteOut)
+async def create_favorite(
+    favorite: FavoriteIn,
+    repo: FavoritesQueries = Depends(),
+    account_data: dict = Depends(authenticator.get_account_data),
+):
+    if "username" in account_data.username:
+        favorites = repo.create(favorite)
+        return favorites
+    else:
+        return "username does not exist"
