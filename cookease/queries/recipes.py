4f8841cccaa9d864e169718714c7a8bdfee695cc
sabina <bahodirova2000@gmail.com>
2023-02-15 17:08:53 -0500

    favorites changes

--- cookease/queries/recipes.py
+++ cookease/queries/recipes.py
@@ -1,16 +1,20 @@
 import requests
 from pydantic import BaseModel
+import os
 
+key = os.environ["SPOONACULAR_API_KEY"]
 
 class Recipe(BaseModel):
     diet: str
     intolerances: str
     includeIngredients: str
-    maxReadyTime: int
+    maxReadyTime: str
 
 
 class RecipeQueries:
     def get_recipe(self, diet: str, intolerances: str, includeIngredients: str, maxReadyTime:int):
-        res = requests.get('https://api.spoonacular.com/recipes/complexSearch?' + diet + intolerances + includeIngredients + maxReadyTime, response_model=Recipe)
+        res = requests.get(
+            'https://api.spoonacular.com/recipes/complexSearch'
+            + diet + intolerances + includeIngredients + maxReadyTime + "apiKey=" + key)
         data = res.json()
         return data
