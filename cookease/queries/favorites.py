233535c073193991eaa11c586e9949d2b66c635e
Braeden Sizemore <bcsize1@gmail.com>
2023-02-16 13:59:53 -0500

    Get Favorite

--- cookease/queries/favorites.py
+++ cookease/queries/favorites.py
@@ -11,25 +11,28 @@ class DuplicateAccountError(ValueError):
 class FavoriteIn(BaseModel):
     recipe_id: int
 
-class FavoriteOut(BaseModel):
-    recipe_id: int
+class FavoriteOut(FavoriteIn):
     user_id: int
-    id: int
+    id: int | str
 
 class FavoriteList(BaseModel):
-    favorites: List[FavoriteOut]
+    favorites: list[FavoriteOut]
+
 
 class FavoritesQueries(Queries):
     DB_NAME = "db-name"
     COLLECTION = "favorites"
 
 
-    def get_favorite(self, recipe_id: int) -> FavoriteOut:
-        favorites = self.collection.find_one({"recipe_id": recipe_id})
-        if not favorites:
-            return None
-        favorites["recipe_id"] = str(favorites["_recipe_id"])
-        return FavoriteOut(**favorites)
+    def get_favorite(self, user_id: int) -> list[FavoriteOut]:
+        results = self.collection.find({"user_id": user_id})
+        print(results, "*********")
+        favorites = []
+        for recipe in results:
+            recipe['recipe_id'] = str(recipe['_recipe_id'])
+            favorite = FavoriteOut(**recipe)
+            favorites.append(favorite)
+        return favorites
 
     def create_favorite(self, favorite: FavoriteIn, recipe_id:int) -> FavoriteOut:
         favorites = favorite.dict()
