141bcf21b2189f1ace4ea568f5d952da2410753c
jennifer <jennifertovar2010@gmail.com>
2023-03-01 18:47:20 -0600

    saving work

--- ghi/src/FavoriteRecipes.js
+++ ghi/src/FavoriteRecipes.js
@@ -1,7 +1,9 @@
 import { useGetFavoriteQuery } from "./auth/api";
-import { useGetTokenQuery} from "./auth/authApi";
+import { useGetTokenQuery } from "./auth/authApi";
 import { Link } from "react-router-dom";
 import RecipeCard from "./RecipeCard";
+import css from "./App.css";
+
 
 function FavoriteRecipes() {
     const { data: tokenData } = useGetTokenQuery()
@@ -12,16 +14,15 @@ function FavoriteRecipes() {
             <progress className="progress is-primary" max="100"></progress>
         )
     }
-    console.log(favorites)
 
     if (!tokenData) {
         return (
-            <div>
-                <h1>Unauthorized</h1>
                 <div>
-                    <Link to='/login'>Login</Link>
+                    <h1>Unauthorized</h1>
+                    <div>
+                        <Link to='/login'>Login</Link>
+                    </div>
                 </div>
-            </div>
         )
     }
 
@@ -34,6 +35,14 @@ function FavoriteRecipes() {
                         <RecipeCard id={recipe.id} title={recipe.title} image={recipe.image} key={recipe.id} />
                     ))}
                 </ul>
+            </div>
+            <div className="card" style="width: 18rem;">
+                <img src="https://tinyurl.com/33vc2n3f" className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <Link href="#" className="btn btn-primary">More Details</Link>
+                    </div>
             </div>
         </>
     )
