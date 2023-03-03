e0fee07d63f60eca29be63d67abf9809334c8b9a
sabina <bahodirova2000@gmail.com>
2023-03-03 13:59:44 -0500

    favorites is authorized

--- ghi/src/FavoriteRecipes.js
+++ ghi/src/FavoriteRecipes.js
@@ -14,54 +14,52 @@ function FavoriteRecipes() {
         )
     }
 
-    if (!tokenData) {
-        return (
-                <div>
-                    <h1>Unauthorized</h1>
-                    <div>
-                        <Link to='/login'>Login</Link>
-                    </div>
-                </div>
-        )
-    }
-
-
     return (
         <>
         <section id="menu" className="menu">
             <div className="container" data-aos="fade-up">
-                <div class="section-header">
+                <div className="section-header">
                     <h2>Cookease</h2>
                     <p>Your <span>Favorite Recipes</span></p>
                 </div>
                 <div className="tab-content" data-aos="fade-up" data-aos-delay="300">
                     <div className="tab-pane fade active show" id="menu-starters">
                         <div className="tab-header text-center">
+                            {tokenData ? (
                             <div className="row gy-5">
-                                {favorites.favorites.map((recipe) => (
+                                {favorites && favorites.favorites.map((recipe) => (
                                 <>
-                                <div className="col-lg-4 menu-item">
-                                    <a href={"recipe-details/" + recipe.id} className="glightbox">
-                                                <img src={recipe.image} className="menu-img img-fluid rounded-circle" alt={recipe.title}/>
-                                    </a>
-                                    <h4>{recipe.title}</h4>
-                                        <ul className="nav nav-tabs d-flex justify-content-center" data-aos="fade-up" data-aos-delay="200">
-                                            <li className="nav-item">
-                                            <a href={"recipe-details/" + recipe.id} className="nav-link active show">
-                                                Read More
-                                            </a>
-                                            </li>
-                                        </ul>
-                                </div>
+                                    <div className="col-lg-4 menu-item">
+                                        <a href={"recipe-details/" + recipe.id} className="glightbox">
+                                                    <img src={recipe.image} className="menu-img img-fluid rounded-circle" alt={recipe.title}/>
+                                        </a>
+                                        <h4>{recipe.title}</h4>
+                                            <ul className="nav nav-tabs d-flex justify-content-center" data-aos="fade-up" data-aos-delay="200">
+                                                <li className="nav-item">
+                                                <a href={"recipe-details/" + recipe.id} className="nav-link active show">
+                                                    Read More
+                                                </a>
+                                                </li>
+                                            </ul>
+                                    </div>
                                 </>
                                 ))}
+                            </div>
+                                ) : (
+                                    <div className="section-header">
+                                        <h4>You must be logged in to see your favorites!</h4>
+                                        <p>Create an account now or sign in!
+                                            <a className="dropdown-item" href="signup">Sign Up</a>
+                                            <a className="dropdown-item" href="login">Log In</a>
+                                        </p>
+                                    </div>
+                                )}
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
-    </>
+            </section>
+        </>
     )
 }
 
