e1fff820626a9195328f86c8ea2681af5ac023ff
sabina <bahodirova2000@gmail.com>
2023-03-07 13:14:33 -0500

    added token to search

--- ghi/src/SearchRecipe.js
+++ ghi/src/SearchRecipe.js
@@ -1,17 +1,10 @@
-<<<<<<< HEAD
 import React, { useState } from 'react';
 import { useLazyGetRecipeQuery, useAddFavoriteRecipeMutation, useDeleteFavoriteMutation, useGetFavoriteQuery } from "./auth/api";
+import { useGetTokenQuery } from './auth/authApi';
 import RecipeCard from './RecipeCard';
 import './assets/vendor/bootstrap-icons/bootstrap-icons.css'
 import './assets/css/main.css';
 import './assets/img/hero-img.png';
-=======
-import { useState } from 'react';
-import { useLazyGetRecipeQuery, useAddFavoriteRecipeMutation, useDeleteFavoriteMutation, useGetFavoriteQuery } from "./auth/api";
-import RecipeCard from './RecipeCard';
-import './assets/vendor/bootstrap-icons/bootstrap-icons.css'
-
->>>>>>> 5ded64629198f3d1ded3c342429b526a69de5d73
 
 function RecipeSearch() {
     const [form, setForm] = useState({
@@ -22,15 +15,10 @@ function RecipeSearch() {
         number: '',
     })
     const [LazyRecipe, { data: lazyData }] = useLazyGetRecipeQuery()
-<<<<<<< HEAD
-    // const { data: favorites, isLoading } = useGetFavoriteQuery()
-    // const [favoriteRecipe] = useAddFavoriteRecipeMutation()
-    // const [unFavoriteRecipe] = useDeleteFavoriteMutation()
-=======
     const { data: favorites, isLoading } = useGetFavoriteQuery()
     const [favoriteRecipe] = useAddFavoriteRecipeMutation()
     const [unFavoriteRecipe] = useDeleteFavoriteMutation()
->>>>>>> 5ded64629198f3d1ded3c342429b526a69de5d73
+    const { data: currentUser } = useGetTokenQuery()
 
 
     const handleSubmit = async (e) => {
@@ -40,32 +28,30 @@ function RecipeSearch() {
     const handleInputChange = (e) => {
         setForm({ ...form, [e.target.name]: e.target.value })
     }
-<<<<<<< HEAD
-    // if (isLoading) {
-    //     return (
-    //         <progress className="progress is-primary" max="100"></progress>
-    //     )
-    // }
 
-    // const handleFavorite = async (id, title, image) => {
-    //     let isFavorite = false;
-    //     let recipe_id;
-    //     for (const recipe of favorites.favorites || []) {
-    //         if (recipe.id === id) {
-    //             isFavorite = true;
-    //             recipe_id = recipe.recipe_id;
-    //             break;
-    //         }
-    //     }
-    //     if (isFavorite) {
-    //         // Remove the recipe from favorites
-    //         await unFavoriteRecipe({ recipe_id });
-    //         console.log("You deleted a recipe")
-    //     } else {
-    //         // Add the recipe to favorites
-    //         await favoriteRecipe({ id, title, image });
-    //     }
-    // }
+    if (isLoading) {
+        return (
+            <progress className="progress is-primary" max="100"></progress>
+        )
+    }
+
+    const handleFavorite = async (id, title, image) => {
+        let isFavorite = false;
+        let recipe_id;
+        for (const recipe of favorites.favorites || []) {
+            if (recipe.id === id) {
+                isFavorite = true;
+                recipe_id = recipe.recipe_id;
+                break;
+            }
+        }
+        if (isFavorite) {
+            await unFavoriteRecipe({ recipe_id });
+            console.log("You deleted a recipe")
+        } else {
+            await favoriteRecipe({ id, title, image });
+        }
+    }
 
     return (
         <div className="row justify-content-center my-5">
@@ -169,116 +155,20 @@ function RecipeSearch() {
                             <div key={pos}>
                                 <div key={recipe.id}>
                                     <RecipeCard id={recipe.id} title={recipe.title} image={recipe.image} key={recipe.id} />
-                                    {/* <button className="btn btn-link" onClick={() => handleFavorite(recipe.id, recipe.title, recipe.image)}>
+                                    {currentUser ? (
+                                    <button className="btn btn-link" onClick={() => handleFavorite(recipe.id, recipe.title, recipe.image)}>
                                             {favorites.favorites?.some(fav => fav.id === recipe.id) ?
                                                 <i className="bi bi-heart-fill heart-icon text-danger"></i> :
                                                 <i className="bi bi-heart heart-icon"></i>
                                             }
-                                        </button> */}
+                                        </button>
+                                    ) : (
+                                        <div></div>
+                                    )}
                                 </div>
                             </div>
                         )}
                     </ul>
-=======
-    if (isLoading) {
-        return (
-            <progress className="progress is-primary" max="100"></progress>
-        )
-    }
-
-    const handleFavorite = async (id, title, image) => {
-        let isFavorite = false;
-        let recipe_id;
-            for (const recipe of favorites.favorites || []) {
-                if (recipe.id === id) {
-                    isFavorite = true;
-                    recipe_id = recipe.recipe_id;
-                    break;
-                }
-            }
-            if (isFavorite) {
-                // Remove the recipe from favorites
-                await unFavoriteRecipe({ recipe_id });
-                console.log("You deleted a recipe")
-            } else {
-                // Add the recipe to favorites
-                await favoriteRecipe({ id, title, image });
-            }
-    }
-
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="diet">Diet:</label>
-                    <input
-                        type="text"
-                        id="diet"
-                        value={form.diet}
-                        name='diet'
-                        onChange={handleInputChange}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="intolerances">Intolerances:</label>
-                    <input
-                        type="text"
-                        id="intolerances"
-                        name='intolerances'
-                        value={form.intolerances}
-                        onChange={handleInputChange}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="includeIngredients">Include Ingredients:</label>
-                    <input
-                        type="text"
-                        name='includeIngredients'
-                        id="includeIngredients"
-                        value={form.includeIngredients}
-                        onChange={handleInputChange}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="maxReadyTime">Max Ready Time:</label>
-                    <input
-                        type="text"
-                        id="maxReadyTime"
-                        name='maxReadyTime'
-                        required
-                        value={form.maxReadyTime}
-                        onChange={handleInputChange}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="number">Number of Results:</label>
-                    <input
-                        type="text"
-                        id="number"
-                        name='number'
-                        value={form.number}
-                        onChange={handleInputChange}
-                    />
-                </div>
-                <button className='btn btn-outline-success' type='submit'>Search</button>
-            </form>
-                <div>
-                <ul>
-                    {lazyData?.results?.map((recipe) => (
-                        <>
-                        <div key={recipe.id}>
-                            <RecipeCard id={recipe.id} title={recipe.title} image={recipe.image} key={recipe.id} />
-                                <button className="btn btn-link" onClick={() => handleFavorite(recipe.id, recipe.title, recipe.image)}>
-                                    {favorites.favorites?.some(fav => fav.id === recipe.id) ?
-                                        <i className="bi bi-heart-fill heart-icon text-danger"></i> :
-                                        <i className="bi bi-heart heart-icon"></i>
-                                    }
-                                </button>
-                        </div>
-                        </>
-                    ))}
-                </ul>
->>>>>>> 5ded64629198f3d1ded3c342429b526a69de5d73
                 </div>
             </div>
         </div>
