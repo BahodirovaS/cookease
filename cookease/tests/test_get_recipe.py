310bde4d5e3575c6b3ff995c66dbc1bd81a3cd89
benjamin <benlhoag@gmail.com>
2023-03-10 17:37:13 -0600

    here is my updated journal

--- cookease/tests/test_get_recipe.py
+++ cookease/tests/test_get_recipe.py
@@ -23,11 +23,19 @@ def test_get_recipe():
 
     # Act
     res = client.get(
+<<<<<<< Updated upstream
         "http://localhost:8000/search-recipes?diet={self.diet}&"
         + "intolerances={self.intolerances}&includeIngredients="
         + "{self.includeIngredients}&maxReadyTime="
         + "{self.maxReadyTime}&number={self.number}"
     )
+=======
+        'http://localhost:8000/search-recipes?diet={self.diet}\
+            &intolerances={self.intolerances}\
+                &includeIngredients={self.includeIngredients}\
+                    &maxReadyTime={self.maxReadyTime}\
+                        &number={self.number}')
+>>>>>>> Stashed changes
     data = res.json()
 
     # Assert
