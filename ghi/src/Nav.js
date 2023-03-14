102999c6954543dd8cf0f6fa059460f42157ca23
sabina <bahodirova2000@gmail.com>
2023-03-14 13:03:15 -0400

    removed /

--- ghi/src/Nav.js
+++ ghi/src/Nav.js
@@ -33,15 +33,15 @@ function Nav() {
                             <div className="dropdown mt-3" style={{ width: '100%', maxWidth: '100vw' }}>
                                 <div>
                                     <NavLink className="dropdown-item" to="/">Home</NavLink>
-                                    <NavLink className="dropdown-item" to="/search-recipes">Search Recipes</NavLink>
-                                    <NavLink className="dropdown-item" to="/favorites-recipes">Users - Loved Recipes</NavLink>
+                                    <NavLink className="dropdown-item" to="search-recipes">Search Recipes</NavLink>
+                                    <NavLink className="dropdown-item" to="favorites-recipes">Users - Loved Recipes</NavLink>
                                     <div>
                                         {currentUser ? (
                                             <button className="dropdown-item" onClick={() => logOut()}>Log Out</button>
                                         ) : (
                                             <>
-                                                <NavLink className="dropdown-item" to="/signup" >Sign Up</NavLink>
-                                                <NavLink className="dropdown-item" to="/login">Log In</NavLink>
+                                                <NavLink className="dropdown-item" to="signup" >Sign Up</NavLink>
+                                                <NavLink className="dropdown-item" to="login">Log In</NavLink>
                                             </>
                                         )}
                                     </div>
