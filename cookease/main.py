f048808edd35fcdc74b54ce33ef6a43403052384
Sabina Bahodirova <bahodirova2000@gmail.com>
2024-06-16 21:03:07 -0400

    added csp middleware to main.py

--- cookease/main.py
+++ cookease/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import os
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.responses import Response
 from authenticator import authenticator
 from routers import router, recipe, favorites
+
+class ContentSecurityPolicyMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request, call_next):
+        response = await call_next(request)
+        response.headers['Content-Security-Policy'] = (
+            "default-src 'self'; "
+            "connect-src 'self' https://git.heroku.com;"
+        )
+        return response
 
 app = FastAPI()
 app.include_router(authenticator.router)
@@ -13,6 +24,7 @@ app.include_router(favorites.router)
 
 app.add_middleware(
     CORSMiddleware,
+    ContentSecurityPolicyMiddleware,
     allow_origins=[
         # os.environ.get("CORS_HOST", "http://localhost:3000"),
         "https://bahodirovas.github.io/",
