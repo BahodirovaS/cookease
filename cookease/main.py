d8616a980d2242156bbf0e83c154f3f950f5fe9c
Sabina Bahodirova <bahodirova2000@gmail.com>
2024-06-18 14:17:32 -0400

    changed allow origins to /coookeas

--- cookease/main.py
+++ cookease/main.py
@@ -26,7 +26,7 @@ app.add_middleware(ContentSecurityPolicyMiddleware)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://bahodirovas.github.io"
+        "https://bahodirovas.github.io/cookease/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
